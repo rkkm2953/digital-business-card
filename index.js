fad0e595b6e117d381a1126352ce0cb76890ebae
rkkm2953 <77224543+rkkm2953@users.noreply.github.com>
2022-06-27 02:01:38 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import './style.css';
-
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-
-root.render(<App />);
